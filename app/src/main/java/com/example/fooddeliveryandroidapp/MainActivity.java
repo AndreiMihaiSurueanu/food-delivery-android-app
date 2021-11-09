56a017311e7075e7ef0bfd303828cb2e43c02873
andrei <git1@email.com>
2021-11-09 18:00:26 +0100

    Display data in PopularFood Recycler view

--- app/src/main/java/com/example/fooddeliveryandroidapp/MainActivity.java
+++ app/src/main/java/com/example/fooddeliveryandroidapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.fooddeliveryandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.fooddeliveryandroidapp.adapter.PopularFoodAdapter;
+import com.example.fooddeliveryandroidapp.model.PopularFood;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView popularRecycler;
+    PopularFoodAdapter popularFoodAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<PopularFood> popularFoodList = new ArrayList<>();
+
+        popularFoodList.add(new PopularFood("Float Cake Vietnam", "$7.05", R.drawable.popularfood1));
+        popularFoodList.add(new PopularFood("Chiken Drumstick", "$17.05", R.drawable.popularfood2));
+        popularFoodList.add(new PopularFood("Fish Tikka Stick", "$25.05", R.drawable.popularfood3));
+        popularFoodList.add(new PopularFood("Float Cake Vietnam", "$7.05", R.drawable.popularfood1));
+        popularFoodList.add(new PopularFood("Chiken Drumstick", "$17.05", R.drawable.popularfood2));
+        popularFoodList.add(new PopularFood("Fish Tikka Stick", "$25.05", R.drawable.popularfood3));
+
+        setPopularRecycler(popularFoodList);
+    }
+
+    private void setPopularRecycler(List<PopularFood> popularFoodList) {
+
+        popularRecycler = findViewById(R.id.popular_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        popularRecycler.setLayoutManager(layoutManager);
+        popularFoodAdapter = new PopularFoodAdapter(this, popularFoodList);
+        popularRecycler.setAdapter(popularFoodAdapter);
+
     }
 }
